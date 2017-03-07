956772c239f1f9a1ec61d05eaf80aab0caf15e5a
ahmed-basyouni <ahmed.basyouni90@gmail.com>
2017-03-07 11:47:39 +0200

    push AndroidKVO

---
+++ AndroidKVO/src/main/java/com/ark/androidkvo/ClassValidator.java
@@ -0,0 +1,46 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Ahmed basyouni
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.ark.androidkvo;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+
+import static javax.lang.model.element.Modifier.ABSTRACT;
+import static javax.lang.model.element.Modifier.PUBLIC;
+
+/**
+ * Helper Class to validate class and fields modifiers
+ * Created by ahmed-basyouni on 12/31/16.
+ */
+
+class ClassValidator {
+
+    static boolean isFieldPublic(Element field){
+        return field.getModifiers().contains(PUBLIC);
+    }
+
+    static boolean isPublic(TypeElement annotatedClass) {
+        return annotatedClass.getModifiers().contains(PUBLIC);
+    }
+
+    static boolean isAbstract(TypeElement annotatedClass) {
+        return annotatedClass.getModifiers().contains(ABSTRACT);
+    }
+
+}
